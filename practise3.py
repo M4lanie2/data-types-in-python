3cf5050107371a5d727736ee62e2c912d3011f5a
M4lanie2 <ekwuemejohn@gmail.com>
2024-12-23 19:13:57 +0000

    Add files via upload

---
+++ practise3.py
@@ -0,0 +1,13 @@
+name="Kaira"
+print(type(name))
+age=11
+print(type(age))
+height=5.4
+print(type(height))
+studentname=input("name: ")
+studentID=int(input("studentID:"))
+print(studentname)
+print(studentID)
+text=input("enter your sting: ")
+revText=text[::-1]
+print(revText)
